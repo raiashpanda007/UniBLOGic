d157e39ae2a21d747e3f990a75236444a85e79d3
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2024-10-16 11:49:29 +0530

    Set up routing from frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} >
 
+    </Route>,
+  ),
+)
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
