5ae804e023c41cd63e1e8bd204ba4bbfa197ab8c
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2024-11-12 15:54:24 +0530

    Updated:server.ts file so that it can upload multimedia files and remove  cors issue

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express'
-
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(cookieParser());
+app.use(cors(
+    {
+        origin: 'http://localhost:5173',
+        credentials: true
+    }
+));
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
     }
