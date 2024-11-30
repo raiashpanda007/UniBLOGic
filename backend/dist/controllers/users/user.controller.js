bb23aaf8a1c78196059ead1099524317c4c52e6e
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2024-11-30 09:49:07 +0530

    update: Folder  structure

--- backend/dist/controllers/users/user.controller.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.showAllUsers = void 0;
-const allusers_1 = __importDefault(require("./allusers"));
-exports.showAllUsers = allusers_1.default;
