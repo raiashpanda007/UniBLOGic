549b08781f2d64af3cad71711e31a6acbbaeb1ba
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2024-12-24 19:49:59 +0530

    added put url backend in our main application

--- backend/Backend Server/src/controllers/post/post.controller.ts
+++ backend/Backend Server/src/controllers/post/post.controller.ts
@@ -2,6 +2,6 @@ import createPost from "./create.post";
 import fetchAllPosts from "./fetchAll.post";
 import upvotePost from "./upvote.post";
 import comment from "./comment.post";
-
+import uploadVideo from "./uploadvideo.post";
 import singlePost from "./fetchSingle.post"
-export { createPost, fetchAllPosts, upvotePost , comment , singlePost};
+export { createPost, fetchAllPosts, upvotePost , comment , singlePost, uploadVideo};
--- backend/Backend Server/src/utilities/utilities.ts
+++ backend/Backend Server/src/utilities/utilities.ts
@@ -3,4 +3,5 @@ import error from './error';
 import response from './response';
 import uploadCloudinary from './fileUpload';
 import deleteSingleFile from './deleteSinglefile';
+
 export { asyncHandler, error, response, uploadCloudinary,deleteSingleFile };
